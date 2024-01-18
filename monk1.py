915494afc2468ac95240be4ec71eb109596d63a5
DavideB45 <dadewww@gmail.com>
2024-01-18 17:56:50 +0100

    trying overfitting monk 1

--- monk1.py
+++ monk1.py
@@ -12,30 +12,38 @@ print(y.shape)
 
 #one hot encode input
 X = feature_one_hot_encoding(X, [3,3,2,3,4,2])
-#y = standard_one_hot_encoding(y, 2)
+y = standard_one_hot_encoding(y, 2)
 
 #split validation set giving 30% of the data to validation
-totData = X.shape[0]
-trainData = int(totData * 0.8)
+'''totData = X.shape[0]
+trainData = int(totData * 0.7)
 ValX = X[trainData:]
 ValY = y[trainData:]
 X = X[:trainData]
 y = y[:trainData]
 print(X.shape)
-print(y.shape)
+print(y.shape)'''
 
-monkModel = NeuralNet([LayerDense(17, 4, ActivationTanH()),
-                          LayerDense(4, 1, ActivationTanH())])
+monkModel = NeuralNet([LayerDense(17, 10, ActivationTanH()),
+                        LayerDense(10, 2, ActivationLinear())])
 
-trError, valError = monkModel.train(X, y, ValX, ValY, learningRate=0.001, epochs=250, batch_size=-1)
+#trError, valError = monkModel.train(X, y, ValX, ValY, learningRate=0.002, epochs=100, batch_size=30)
+trError, valError = monkModel.train(X, y, learningRate=0.003, epochs=300, batch_size=30, lambdaRegularization=0.0001)
 
 #compute accuracy
-from metrics import accuracy_classifier_single_output as accuracy
+from metrics import LossMSE, accuracy_classifier_multiple_output as accuracy
 y_predicted = monkModel.forward(X)
 print("training Accuracy: ", accuracy(y, y_predicted))
-y_predicted = monkModel.forward(ValX)
-print("validation Accuracy: ", accuracy(ValY, y_predicted))
+#y_predicted = monkModel.forward(ValX)
+#print("validation Accuracy: ", accuracy(ValY, y_predicted))
 
+#check test error
+'''X, y = readMonkData("data/monk/monks-1.test")
+X = feature_one_hot_encoding(X, [3,3,2,3,4,2])
+y = standard_one_hot_encoding(y, 2)
+y_predicted = monkModel.forward(X)
+print("Test Accuracy: ", accuracy(y, y_predicted))
+print("Test Loss: ", LossMSE(y, y_predicted))   '''
 
 import matplotlib.pyplot as plt
 plt.plot(trError, label="Training error")
@@ -44,7 +52,7 @@ plt.legend()
 plt.show()
 
 #show some examples from the dataset
-start = 10
+'''start = 10
 end = start + 10
 new_X = X[start:end]
 new_y = y[start:end]
@@ -53,4 +61,15 @@ for i in range(len(new_X)):
     print("*************")
     print('new example: ', new_X[i])
     print('expected: ', new_y[i])
-    print('predicted: ', y_predicted[i])
+    print('predicted: ', y_predicted[i])'''
+
+positive = 0
+negative = 0
+for i in range(len(X)):
+    if np.argmax(y[i]) == 1:
+        positive += 1
+    else:
+        negative += 1
+
+#print("positive: ", positive)
+#print("negative: ", negative)
