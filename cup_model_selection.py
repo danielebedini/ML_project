6b3ea83d67959ea0ad63067f3eba2cc3766c6f4d
DavideB45 <dadewww@gmail.com>
2024-01-25 11:39:34 +0100

    upadated cup ms

--- cup_model_selection.py
+++ cup_model_selection.py
@@ -2,7 +2,7 @@ from activations import *
 from dataRescaling import DataProcessor
 from layers import LayerDense
 from learningRate import LearningRate
-from metrics import MEE, LossMSE
+from metrics import MEE, LossMSE, rSquare
 from net import NeuralNet
 from r_prop_parameter import RProp
 from utilities import plot_data_error, readTrainingCupData
@@ -31,47 +31,65 @@ nn_cup = NeuralNet([LayerDense(10, 30, ActivationTanH(standardInit=standardInit)
                     LayerDense(15, 3, ActivationLinear())
                     ])
 
-validator = Validator(nn_cup, X, y, MEE, showPlot=False)#important: use MEE as loss function
-trErr, valErr, trErrDev, valErrDev, _, _ = validator.kfold(k=20,
-                                                            epochs=200,
-                                                            lambdaRegularization=0.0,
-                                                            patience=-1,
-                                                            r_prop=RProp(),
-                                                            outputProcessor=DataProcessor(y, standardize=True, independentColumns=True)
-                                                            )
+'''
+dataProcessor = DataProcessor(y, standardize=True, independentColumns=True)
+y = dataProcessor.process(y)
+nn_cup.train(X, y,
+            epochs=200,
+            r_prop=RProp(),
+            patience=-1,
+            lambdaRegularization=0.0,
+            )
+
+#evaluate
+y_pred = nn_cup.forward(X)
+y_pred = dataProcessor.deprocess(y_pred)
+y = dataProcessor.deprocess(y)
+print("MEE: ", MEE(y, y_pred))
+print("R Squared: ", rSquare(y, y_pred))
+'''
+
+validator = Validator(nn_cup, X, y, MEE, rSquare, showPlot=False)#important: use MEE as loss function
+trErr, valErr, trErrDev, valErrDev, trAcc, valAcc, trAccDev, valAccDev = validator.kfold(k=20,
+                                                                                        epochs=200,
+                                                                                        lambdaRegularization=0.0,
+                                                                                        patience=-1,
+                                                                                        r_prop=RProp(),
+                                                                                        outputProcessor=DataProcessor(y, standardize=True, independentColumns=True)
+                                                                                        )
 
 print("|--------- Standardize Data ---------|")
-print("TrErr: ", trErr)
-print("ValErr: ", valErr)
-print("TrErrDev: ", trErrDev)
-print("ValErrDev: ", valErrDev)
+print(f"TrErr:  {'%.4f' % trErr} ± {'%.4f' % trErrDev}")
+print(f"ValErr: {'%.4f' % valErr} ± {'%.4f' % valErrDev}")
+print(f"TrAcc:  {'%.4f' % trAcc} ± {'%.4f' % trAccDev}")
+print(f"ValAcc: {'%.4f' % valAcc} ± {'%.4f' % valAccDev}")
 
-trErr, valErr, trErrDev, valErrDev, _, _ = validator.kfold(k=20,
-                                                            epochs=200,
-                                                            lambdaRegularization=0.0,
-                                                            patience=-1,
-                                                            r_prop=RProp(),
-                                                            outputProcessor=DataProcessor(y, normalize=True, independentColumns=True)
-                                                            )
+trErr, valErr, trErrDev, valErrDev, trAcc, valAcc, trAccDev, valAccDev = validator.kfold(k=20,
+                                                                                        epochs=200,
+                                                                                        lambdaRegularization=0.0,
+                                                                                        patience=-1,
+                                                                                        r_prop=RProp(),
+                                                                                        outputProcessor=DataProcessor(y, normalize=True, independentColumns=True)
+                                                                                        )
 
 
 print("|---------  Normalize Data  ---------|")
-print("TrErr: ", trErr)
-print("ValErr: ", valErr)
-print("TrErrDev: ", trErrDev)
-print("ValErrDev: ", valErrDev)
+print(f"TrErr:  {'%.4f' % trErr} ± {'%.4f' % trErrDev}")
+print(f"ValErr: {'%.4f' % valErr} ± {'%.4f' % valErrDev}")
+print(f"TrAcc:  {'%.4f' % trAcc} ± {'%.4f' % trAccDev}")
+print(f"ValAcc: {'%.4f' % valAcc} ± {'%.4f' % valAccDev}")
 
-trErr, valErr, trErrDev, valErrDev, _, _ = validator.kfold(k=20,
-                                                            epochs=200,
-                                                            lambdaRegularization=0.0,
-                                                            patience=-1,
-                                                            r_prop=RProp(),
-                                                            outputProcessor=None
-                                                            )
+trErr, valErr, trErrDev, valErrDev, trAcc, valAcc, trAccDev, valAccDev = validator.kfold(k=20,
+                                                                                        epochs=200,
+                                                                                        lambdaRegularization=0.0,
+                                                                                        patience=-1,
+                                                                                        r_prop=RProp(),
+                                                                                        outputProcessor=None
+                                                                                        )
 
 
 print("|---------     Raw Data     ---------|")
-print("TrErr: ", trErr)
-print("ValErr: ", valErr)
-print("TrErrDev: ", trErrDev)
-print("ValErrDev: ", valErrDev)
+print(f"TrErr:  {'%.4f' % trErr} ± {'%.4f' % trErrDev}")
+print(f"ValErr: {'%.4f' % valErr} ± {'%.4f' % valErrDev}")
+print(f"TrAcc:  {'%.4f' % trAcc} ± {'%.4f' % trAccDev}")
+print(f"ValAcc: {'%.4f' % valAcc} ± {'%.4f' % valAccDev}")
