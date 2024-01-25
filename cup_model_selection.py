3df71f3d1974f065696fd51db07f506a7b1bbaf4
DavideB45 <dadewww@gmail.com>
2024-01-25 19:41:06 +0100

    result analyzer

--- cup_model_selection.py
+++ cup_model_selection.py
@@ -1,5 +1,6 @@
 from activations import *
 from dataRescaling import DataProcessor
+from grid_search import grid_search_RProp, grid_search_momentum
 from layers import LayerDense
 from learningRate import LearningRate
 from metrics import MEE, LossMSE, rSquare
@@ -10,9 +11,9 @@ from validation import Validator
 
 X, y = readTrainingCupData("data/cup/ML-CUP23-TR.csv")
 
-#take only 50% of test data
-X = X[:int(len(X)*0.5)] #WARNING: do not touch this line
-y = y[:int(len(y)*0.5)] #WARNING: do not touch this line
+#take only 75% of the data for training and validation (the remaining 25% will be used for testing)
+X = X[:int(len(X)*0.75)] #WARNING: do not touch this line
+y = y[:int(len(y)*0.75)] #WARNING: do not touch this line
 
 
 #####################################################################
@@ -23,38 +24,58 @@ y = y[:int(len(y)*0.5)] #WARNING: do not touch this line
 
 
 
-standardInit = False
-nn_cup = NeuralNet([LayerDense(10, 30, ActivationTanH(standardInit=standardInit)),
-                    LayerDense(30, 25, ActivationTanH(standardInit=standardInit)),
-                    LayerDense(25, 20, ActivationTanH(standardInit=standardInit)),
-                    LayerDense(20, 15, ActivationTanH(standardInit=standardInit)),
+
+nn_cup1 = NeuralNet([LayerDense(10, 30, ActivationTanH()),
+                    LayerDense(30, 15, ActivationTanH()),
                     LayerDense(15, 3, ActivationLinear())
-                    ])
+                    ],
+                    name="tanH_30_15_3")
+
+nn_cup2 = NeuralNet([LayerDense(10, 40, ActivationSigmoid()),
+                    LayerDense(40, 3, ActivationLinear())
+                    ],
+                    name="sigmoid_40_3")
+
+nn_cup3 = NeuralNet([LayerDense(10, 40, ActivationTanH()),
+                    LayerDense(40, 3, ActivationLinear())
+                    ],
+                    name="tanH_40_3")
+
 
 '''
-dataProcessor = DataProcessor(y, standardize=True, independentColumns=True)
-y = dataProcessor.process(y)
-nn_cup.train(X, y,
-            epochs=200,
-            r_prop=RProp(),
-            patience=-1,
-            lambdaRegularization=0.0,
-            )
-
-#evaluate
-y_pred = nn_cup.forward(X)
-y_pred = dataProcessor.deprocess(y_pred)
-y = dataProcessor.deprocess(y)
-print("MEE: ", MEE(y, y_pred))
-print("R Squared: ", rSquare(y, y_pred))
+grid_search_RProp(X, y, 10,
+                  {
+                        'epochs': 500,
+                        'model': [nn_cup1, nn_cup2, nn_cup3],
+                        'delta_0': [0.01, 0.05],
+                        'delta_max': [10],
+                        'lambdaRegularization': [0.0008, 0.0009, 0.001, 0.002],
+                        'preprocess': ['standardize', 'normalize', None],
+                        'standardInit': [True, False]
+                  }
+)
 '''
 
-validator = Validator(nn_cup, X, y, MEE, rSquare, showPlot=False)#important: use MEE as loss function
-trErr, valErr, trErrDev, valErrDev, trAcc, valAcc, trAccDev, valAccDev = validator.kfold(k=20,
-                                                                                        epochs=200,
-                                                                                        lambdaRegularization=0.0,
-                                                                                        patience=-1,
-                                                                                        r_prop=RProp(),
+grid_search_momentum(X, y, 10,
+                    {
+                            'epochs': 4000,
+                            'model': [nn_cup1, nn_cup2, nn_cup3],
+                            'momentum': [0.9, 0.95, 0.99],
+                            'learningRate': [LearningRate(0.3), LearningRate(0.2), LearningRate(0.1)],
+                            'batch_size': [80, 110, 120],
+                            'lambdaRegularization': [0.0007, 0.0008, 0.0009, 0.001],
+                            'preprocess': ['standardize'],
+                            'standardInit': [True, False]
+                    }
+    )
+exit()
+validator = Validator(nn_cup3, X, y, MEE, rSquare, showPlot=True)#important: use MEE as loss function
+trErr, valErr, trErrDev, valErrDev, trAcc, valAcc, trAccDev, valAccDev = validator.kfold(k=2,
+                                                                                        epochs=4000,
+                                                                                        lambdaRegularization=0.0008,
+                                                                                        patience=10,
+                                                                                        momentum=0.9,
+                                                                                        learningRate=LearningRate(0.1),
                                                                                         outputProcessor=DataProcessor(y, standardize=True, independentColumns=True)
                                                                                         )
 
@@ -63,7 +84,7 @@ print(f"TrErr:  {'%.4f' % trErr} ± {'%.4f' % trErrDev}")
 print(f"ValErr: {'%.4f' % valErr} ± {'%.4f' % valErrDev}")
 print(f"TrAcc:  {'%.4f' % trAcc} ± {'%.4f' % trAccDev}")
 print(f"ValAcc: {'%.4f' % valAcc} ± {'%.4f' % valAccDev}")
-
+exit()
 trErr, valErr, trErrDev, valErrDev, trAcc, valAcc, trAccDev, valAccDev = validator.kfold(k=20,
                                                                                         epochs=200,
                                                                                         lambdaRegularization=0.0,
@@ -93,3 +114,5 @@ print(f"TrErr:  {'%.4f' % trErr} ± {'%.4f' % trErrDev}")
 print(f"ValErr: {'%.4f' % valErr} ± {'%.4f' % valErrDev}")
 print(f"TrAcc:  {'%.4f' % trAcc} ± {'%.4f' % trAccDev}")
 print(f"ValAcc: {'%.4f' % valAcc} ± {'%.4f' % valAccDev}")
+
+
---
+++ grid_result_analyzer.py
@@ -0,0 +1,44 @@
+import json
+import numpy as np
+
+name = 'grid_search_results_RProp.json'
+name2 = 'grid_search_results_momentum.json'
+
+chosen = name2
+
+file = open(chosen, 'r')
+results = json.load(file)[1]
+file.close()
+
+#   x['valErrDev']
+#   x['valE']
+top = sorted(results, key=lambda x: x['valE'])[:20]
+
+if chosen == name:
+    for i in range(len(top)):
+        print(f"val MEE: {'%.4f' % top[i]['valE']} +/- {'%.4f' % top[i]['valErrDev']} | \
+val R^2: {'%.4f' % top[i]['valA']} +/- {'%.4f' % top[i]['valAccDev']} | \
+tr MEE: {'%.4f' % top[i]['trE']} +/- {'%.4f' % top[i]['trErrDev']} | \
+tr R^2: {'%.4f' % top[i]['trA']} +/- {'%.4f' % top[i]['trAccDev']} | \
+preprocess: {top[i]['preprocess']} | \
+lambda: {top[i]['lambdaRegularization']} | \
+delta_max: {top[i]['delta_max']} | \
+delta_0: {top[i]['delta_0']} | \
+standardInit: {top[i]['standardInit']} | \
+model: {top[i]['model']}\
+")
+        
+else:
+    for i in range(len(top)):
+        print(f"v MEE: {'%.4f' % top[i]['valE']} +/- {'%.4f' % top[i]['valErrDev']} | \
+v R^2: {'%.4f' % top[i]['valA']} +/- {'%.4f' % top[i]['valAccDev']} | \
+t MEE: {'%.4f' % top[i]['trE']} +/- {'%.4f' % top[i]['trErrDev']} | \
+t R^2: {'%.4f' % top[i]['trA']} +/- {'%.4f' % top[i]['trAccDev']} | \
+preprocess: {top[i]['preprocess']} | \
+lambda: {top[i]['lambdaRegularization']} | \
+alpha: {top[i]['momentum']} | \
+eta: {top[i]['learningRate']} | \
+batch_size: {top[i]['batch_size']} | \
+standardInit: {top[i]['standardInit']} | \
+model: {top[i]['model']}\
+")
